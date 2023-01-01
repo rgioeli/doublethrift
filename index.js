43bb8c277ec694fb6ee71b7242327864920b1739
rgioeli <rgioeli@icloud.com>
2023-01-01 13:19:05 -0500

    updated port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 dotenv.config();
